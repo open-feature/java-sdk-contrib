f1a6461bbed101138c50b0ca7b1a97c4d4417de1
Todd Baert <toddbaert@gmail.com>
2022-08-30 00:08:37 -0400

    fix: test fix publish again
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- hooks/open-telemetry/src/test/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHookTest.java
+++ hooks/open-telemetry/src/test/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHookTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class OpenTelemetryHookTest {
 
     @Test
-    @DisplayName("a simple test!")
+    @DisplayName("a simple test")
     void test() {
         assertThat(OpenTelemetryHook.test()).isEqualTo(true);
     }
