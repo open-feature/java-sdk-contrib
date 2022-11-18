022d8c4039c2afd78511cb307b9afe68a382bf6a
Todd Baert <toddbaert@gmail.com>
2022-11-18 16:22:49 -0500

    feat: flagd-provider ssl, socket, deadline, env support
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/FlagdProvider.java
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/FlagdProvider.java
@@ -1,9 +1,13 @@
 package dev.openfeature.contrib.providers.flagd;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
+import javax.net.ssl.SSLException;
 
 import dev.openfeature.flagd.grpc.Schema.ResolveBooleanRequest;
 import dev.openfeature.flagd.grpc.Schema.ResolveBooleanResponse;
@@ -23,9 +27,13 @@ import dev.openfeature.sdk.Metadata;
 import dev.openfeature.sdk.MutableStructure;
 import dev.openfeature.sdk.ProviderEvaluation;
 import dev.openfeature.sdk.Value;
-import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NettyChannelBuilder;
+import io.netty.channel.epoll.EpollDomainSocketChannel;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.unix.DomainSocketAddress;
+import io.netty.handler.ssl.SslContextBuilder;
 import lombok.extern.slf4j.Slf4j;
-
 
 /**
  * OpenFeature provider for flagd.
@@ -33,42 +41,52 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FlagdProvider implements FeatureProvider {
 
-    private ServiceBlockingStub serviceStub;
     static final String PROVIDER_NAME = "flagD Provider";
-    static final int DEFAULT_PORT = 8013;
-    static final String DEFAULT_HOST = "localhost"; 
-    
+    static final String DEFAULT_PORT = "8013";
+    static final String DEFAULT_TLS = "false";
+    static final String DEFAULT_HOST = "localhost";
+    static final int DEFAULT_DEADLINE = 500;
+
+    static final String HOST_ENV_VAR_NAME = "FLAGD_HOST";
+    static final String PORT_ENV_VAR_NAME = "FLAGD_PORT";
+    static final String TLS_ENV_VAR_NAME = "FLAGD_TLS";
+    static final String SOCKET_PATH_ENV_VAR_NAME = "FLAGD_SOCKET_PATH";
+    static final String SERVER_CERT_PATH_ENV_VAR_NAME = "FLAGD_SERVER_CERT_PATH";
+
+    private long deadline = DEFAULT_DEADLINE;
+    private ServiceBlockingStub serviceStub;
+
     /**
      * Create a new FlagdProvider instance.
      *
-     * @param protocol transport protocol, "http" or "https"
-     * @param host     flagd host, defaults to localhost
-     * @param port     flagd port, defaults to 8013
+     * @param socketPath unix socket path
      */
-    public FlagdProvider(Protocol protocol, String host, int port) {
-        
-        this(Protocol.HTTPS == protocol 
-            ? ServiceGrpc.newBlockingStub(ManagedChannelBuilder.forAddress(host, port)
-                .useTransportSecurity()
-                .build()) :
-            ServiceGrpc.newBlockingStub(ManagedChannelBuilder.forAddress(host, port)
-                .usePlaintext()
-                .build()));
+    public FlagdProvider(String socketPath) {
+        this(buildServiceStub(null, null, null, null, socketPath));
+    }
+
+    /**
+     * Create a new FlagdProvider instance.
+     *
+     * @param host     flagd server host, defaults to "localhost"
+     * @param port     flagd server port, defaults to 8013
+     * @param tls      use TLS, defaults to false
+     * @param certPath path for server certificate, defaults to null to, using
+     *                 system certs
+     *
+     */
+    public FlagdProvider(String host, int port, boolean tls, String certPath) {
+        this(buildServiceStub(host, port, tls, certPath, null));
     }
 
     /**
      * Create a new FlagdProvider instance.
      */
     public FlagdProvider() {
-        this(Protocol.HTTP, DEFAULT_HOST, DEFAULT_PORT);
+        this(buildServiceStub(null, null, null, null, null));
     }
 
-    /**
-     * Create a new FlagdProvider instance.
-     *
-     * @param serviceStub service stub instance to use
-     */
-    public FlagdProvider(ServiceBlockingStub serviceStub) {
+    FlagdProvider(ServiceBlockingStub serviceStub) {
         this.serviceStub = serviceStub;
     }
 
@@ -84,76 +102,92 @@ public class FlagdProvider implements FeatureProvider {
 
     @Override
     public ProviderEvaluation<Boolean> getBooleanEvaluation(String key, Boolean defaultValue,
-        EvaluationContext ctx) {
+            EvaluationContext ctx) {
 
         ResolveBooleanRequest request = ResolveBooleanRequest.newBuilder()
-            .setFlagKey(key)
-            .setContext(this.convertContext(ctx))
-            .build();
-        ResolveBooleanResponse r = this.serviceStub.resolveBoolean(request);
+                .setFlagKey(key)
+                .setContext(this.convertContext(ctx))
+                .build();
+        ResolveBooleanResponse r = this.serviceStub.withDeadlineAfter(this.deadline, TimeUnit.MILLISECONDS)
+                .resolveBoolean(request);
         return ProviderEvaluation.<Boolean>builder()
-            .value(r.getValue())
-            .variant(r.getVariant())
-            .reason(r.getReason())
-            .build();
+                .value(r.getValue())
+                .variant(r.getVariant())
+                .reason(r.getReason())
+                .build();
     }
 
     @Override
     public ProviderEvaluation<String> getStringEvaluation(String key, String defaultValue,
-        EvaluationContext ctx) {
+            EvaluationContext ctx) {
         ResolveStringRequest request = ResolveStringRequest.newBuilder()
-            .setFlagKey(key)
-            .setContext(this.convertContext(ctx)).build();
-        ResolveStringResponse r = this.serviceStub.resolveString(request);
+                .setFlagKey(key)
+                .setContext(this.convertContext(ctx)).build();
+        ResolveStringResponse r = this.serviceStub.withDeadlineAfter(this.deadline, TimeUnit.MILLISECONDS)
+                .resolveString(request);
         return ProviderEvaluation.<String>builder().value(r.getValue())
-            .variant(r.getVariant())
-            .reason(r.getReason())
-            .build();
+                .variant(r.getVariant())
+                .reason(r.getReason())
+                .build();
     }
 
     @Override
     public ProviderEvaluation<Double> getDoubleEvaluation(String key, Double defaultValue,
-        EvaluationContext ctx) {
+            EvaluationContext ctx) {
         ResolveFloatRequest request = ResolveFloatRequest.newBuilder()
-            .setFlagKey(key)
-            .setContext(this.convertContext(ctx))
-            .build();
-        ResolveFloatResponse r = this.serviceStub.resolveFloat(request);
+                .setFlagKey(key)
+                .setContext(this.convertContext(ctx))
+                .build();
+        ResolveFloatResponse r = this.serviceStub.withDeadlineAfter(this.deadline, TimeUnit.MILLISECONDS)
+                .resolveFloat(request);
         return ProviderEvaluation.<Double>builder()
-            .value(r.getValue())
-            .variant(r.getVariant())
-            .reason(r.getReason())
-            .build();
+                .value(r.getValue())
+                .variant(r.getVariant())
+                .reason(r.getReason())
+                .build();
     }
 
     @Override
     public ProviderEvaluation<Integer> getIntegerEvaluation(String key, Integer defaultValue,
-        EvaluationContext ctx) {
+            EvaluationContext ctx) {
         ResolveIntRequest request = ResolveIntRequest.newBuilder()
-            .setFlagKey(key)
-            .setContext(this.convertContext(ctx))
-            .build();
-        ResolveIntResponse r = this.serviceStub.resolveInt(request);
+                .setFlagKey(key)
+                .setContext(this.convertContext(ctx))
+                .build();
+        ResolveIntResponse r = this.serviceStub.withDeadlineAfter(this.deadline, TimeUnit.MILLISECONDS)
+                .resolveInt(request);
         return ProviderEvaluation.<Integer>builder()
-            .value((int) r.getValue())
-            .variant(r.getVariant())
-            .reason(r.getReason())
-            .build();
+                .value((int) r.getValue())
+                .variant(r.getVariant())
+                .reason(r.getReason())
+                .build();
     }
 
     @Override
     public ProviderEvaluation<Value> getObjectEvaluation(String key, Value defaultValue,
-        EvaluationContext ctx) {
+            EvaluationContext ctx) {
         ResolveObjectRequest request = ResolveObjectRequest.newBuilder()
-            .setFlagKey(key)
-            .setContext(this.convertContext(ctx))
-            .build();
-        ResolveObjectResponse r = this.serviceStub.resolveObject(request);
+                .setFlagKey(key)
+                .setContext(this.convertContext(ctx))
+                .build();
+        ResolveObjectResponse r = this.serviceStub.withDeadlineAfter(this.deadline, TimeUnit.MILLISECONDS)
+                .resolveObject(request);
         return ProviderEvaluation.<Value>builder()
-            .value(this.convertObjectResponse(r.getValue()))
-            .variant(r.getVariant())
-            .reason(r.getReason())
-            .build();
+                .value(this.convertObjectResponse(r.getValue()))
+                .variant(r.getVariant())
+                .reason(r.getReason())
+                .build();
+    }
+
+    /**
+     * Sets how long to wait for an evaluation. 
+     *
+     * @param deadlineMs time to wait before gRPC call is cancelled. Defaults to 10ms.
+     * @return FlagdProvider
+     */
+    FlagdProvider setDeadline(long deadlineMs) {
+        this.deadline = deadlineMs;
+        return this;
     }
 
     /**
@@ -207,7 +241,7 @@ public class FlagdProvider implements FeatureProvider {
             values.put(key, this.convertAny(value));
         });
         com.google.protobuf.Struct struct = com.google.protobuf.Struct.newBuilder()
-            .putAllFields(values).build();
+                .putAllFields(values).build();
         return com.google.protobuf.Value.newBuilder().setStructValue(struct).build();
     }
 
@@ -225,12 +259,13 @@ public class FlagdProvider implements FeatureProvider {
     }
 
     /**
-     * Convert openfeature list to protobuf list. 
+     * Convert openfeature list to protobuf list.
      */
     private com.google.protobuf.Value convertList(List<Value> values) {
         com.google.protobuf.ListValue list = com.google.protobuf.ListValue.newBuilder()
-            .addAllValues(values.stream()
-            .map(v -> this.convertAny(v)).collect(Collectors.toList())).build();
+                .addAllValues(values.stream()
+                        .map(v -> this.convertAny(v)).collect(Collectors.toList()))
+                .build();
         return com.google.protobuf.Value.newBuilder().setListValue(list).build();
     }
 
@@ -274,5 +309,52 @@ public class FlagdProvider implements FeatureProvider {
             value = new Value();
         }
         return value;
+    }
+
+    private static ServiceBlockingStub buildServiceStub(String host, Integer port, Boolean tls, String certPath,
+            String socketPath) {
+
+        host = host != null ? host : fallBackToEnvOrDefault(HOST_ENV_VAR_NAME, DEFAULT_HOST);
+        port = port != null ? port : Integer.parseInt(fallBackToEnvOrDefault(PORT_ENV_VAR_NAME, DEFAULT_PORT));
+        tls = tls != null ? tls : Boolean.parseBoolean(fallBackToEnvOrDefault(TLS_ENV_VAR_NAME, DEFAULT_TLS));
+        certPath = certPath != null ? certPath : fallBackToEnvOrDefault(SERVER_CERT_PATH_ENV_VAR_NAME, null);
+        socketPath = socketPath != null ? socketPath : fallBackToEnvOrDefault(SOCKET_PATH_ENV_VAR_NAME, null);
+
+        // we have a socket path specified, build a channel with a unix socket
+        if (socketPath != null) {
+            return ServiceGrpc.newBlockingStub(NettyChannelBuilder
+                    .forAddress(new DomainSocketAddress(socketPath))
+                    .eventLoopGroup(new EpollEventLoopGroup())
+                    .channelType(EpollDomainSocketChannel.class)
+                    .usePlaintext()
+                    .build());
+        }
+
+        // build a TCP socket
+        try {
+            NettyChannelBuilder builder = NettyChannelBuilder
+                    .forAddress(host, port);
+
+            if (tls) {
+                SslContextBuilder sslContext = GrpcSslContexts.forClient();
+                if (certPath != null) {
+                    sslContext.trustManager(new File(certPath));
+                }
+                builder.sslContext(sslContext.build());
+            } else {
+                builder.usePlaintext();
+            }
+
+            return ServiceGrpc
+                    .newBlockingStub(builder.build());
+        } catch (SSLException ssle) {
+            SslConfigException sslConfigException = new SslConfigException("Error with SSL configuration.");
+            sslConfigException.initCause(ssle);
+            throw sslConfigException;
+        }
+    }
+
+    private static String fallBackToEnvOrDefault(String key, String defaultValue) {
+        return System.getenv(key) != null ? System.getenv(key) : defaultValue;
     }
 }
