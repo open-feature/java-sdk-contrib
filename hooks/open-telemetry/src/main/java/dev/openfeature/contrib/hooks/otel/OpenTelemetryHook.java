a38f5ae975a95fe78ba8bdc372667dbe0043dd90
thiyagu06 <thiyagu103@gmail.com>
2022-12-18 16:02:14 +0530

    feat!: update otel hook to be spec compliant
    Signed-off-by: thiyagu06 <thiyagu103@gmail.com>

--- hooks/open-telemetry/src/main/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHook.java
+++ hooks/open-telemetry/src/main/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHook.java
@@ -1,29 +1,79 @@
 package dev.openfeature.contrib.hooks.otel;
 
-import dev.openfeature.sdk.Client;
-import dev.openfeature.sdk.NoOpProvider;
-import dev.openfeature.sdk.OpenFeatureAPI;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.openfeature.sdk.FlagEvaluationDetails;
+import dev.openfeature.sdk.Hook;
+import dev.openfeature.sdk.HookContext;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import lombok.SneakyThrows;
 
-/** 
- * A placeholder.
+import java.util.Map;
+
+/**
+ * The OpenTelemetry hook  provides a way to automatically add a feature flag evaluation to a span as a span event.
+ * Refer to <a href="https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/feature-flags.md">OpenTelemetry</a>
  */
-public class OpenTelemetryHook {
-    
-    /** 
+public class OpenTelemetryHook implements Hook {
+
+    private final AttributeKey<String> flagKeyAttributeKey = AttributeKey.stringKey("feature_flag.flag_key");
+
+    private final AttributeKey<String> providerNameAttributeKey = AttributeKey.stringKey("feature_flag.provider_name");
+
+    private final AttributeKey<String> variantAttributeKey = AttributeKey.stringKey("feature_flag.variant");
+
+    private static final String EVENT_NAME = "feature_flag";
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
      * Create a new OpenTelemetryHook instance.
      */
     public OpenTelemetryHook() {
     }
 
-    /** 
-     * A test method...
+    /**
+     * Records the event in the current span after the successful flag evaluation.
      *
-     * @return {boolean}
+     * @param ctx Information about the particular flag evaluation
+     * @param details Information about how the flag was resolved, including any resolved values.
+     * @param hints  An immutable mapping of data for users to communicate to the hooks.
      */
-    public static boolean test() {
-        OpenFeatureAPI.getInstance().setProvider(new NoOpProvider());
-        Client client = OpenFeatureAPI.getInstance().getClient();
-        return client.getBooleanValue("test2", true);
+    @SneakyThrows
+    @Override
+    public void after(HookContext ctx, FlagEvaluationDetails details, Map hints) {
+        Span currentSpan = Span.current();
+        String value = details.getValue() != null ? getValue(details.getValue()) : "";
+        if (currentSpan != null) {
+            Attributes attributes = Attributes.of(
+                    flagKeyAttributeKey, ctx.getFlagKey(),
+                    providerNameAttributeKey, ctx.getProviderMetadata().getName(),
+                    variantAttributeKey, value);
+            currentSpan.addEvent(EVENT_NAME, attributes);
+        }
     }
 
+    /**
+     * Records the error details in the current span after the flag evaluation has processed abnormally.
+     *
+     * @param ctx   Information about the particular flag evaluation
+     * @param error The exception that was thrown.
+     * @param hints An immutable mapping of data for users to communicate to the hooks.
+     */
+    @Override
+    public void error(HookContext ctx, Exception error, Map hints) {
+        Span currentSpan = Span.current();
+        if (currentSpan != null) {
+            Attributes attributes = Attributes.of(
+                    flagKeyAttributeKey, ctx.getFlagKey(),
+                    providerNameAttributeKey, ctx.getProviderMetadata().getName());
+            currentSpan.recordException(error, attributes);
+        }
+    }
+
+    @SneakyThrows
+    private String getValue(Object value) {
+        return objectMapper.writeValueAsString(value);
+    }
 }
--- hooks/open-telemetry/src/test/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHookTest.java
+++ hooks/open-telemetry/src/test/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHookTest.java
@@ -1,15 +1,116 @@
 package dev.openfeature.contrib.hooks.otel;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import dev.openfeature.sdk.FlagEvaluationDetails;
+import dev.openfeature.sdk.FlagValueType;
+import dev.openfeature.sdk.HookContext;
+import dev.openfeature.sdk.MutableContext;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Any;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+@ExtendWith(MockitoExtension.class)
 class OpenTelemetryHookTest {
 
-    @Test
-    @DisplayName("a simple test.")
-    void test() {
-        assertThat(OpenTelemetryHook.test()).isEqualTo(true);
+    private OpenTelemetryHook openTelemetryHook = new OpenTelemetryHook();
+
+    private final AttributeKey<String> flagKeyAttributeKey = AttributeKey.stringKey("feature_flag.flag_key");
+
+    private final AttributeKey<String> providerNameAttributeKey = AttributeKey.stringKey("feature_flag.provider_name");
+
+    private final AttributeKey<String> variantAttributeKey = AttributeKey.stringKey("feature_flag.variant");
+
+    private static MockedStatic<Span> mockedSpan;
+
+    @Mock private Span span;
+
+    private HookContext<String> hookContext = HookContext.<String>builder()
+            .flagKey("test_key")
+            .type(FlagValueType.STRING)
+            .providerMetadata(() -> "test provider")
+            .ctx(new MutableContext())
+            .defaultValue("default")
+            .build();
+
+    @BeforeAll
+    public static void init() {
+        mockedSpan = mockStatic(Span.class);
     }
+
+    @AfterAll
+    public static void close() {
+        mockedSpan.close();
+    }
+
+    @Test
+    @DisplayName("should add an event in span during after method execution")
+    void should_add_event_in_span_during_after_method_execution() {
+        FlagEvaluationDetails<String> details = FlagEvaluationDetails.<String>builder()
+                .variant("test_variant")
+                .value("variant passed")
+                .build();
+        mockedSpan.when(Span::current).thenReturn(span);
+        openTelemetryHook.after(hookContext, details, null);
+        Attributes expectedAttr = Attributes.of(flagKeyAttributeKey, "test_key",
+                providerNameAttributeKey, "test provider",
+                variantAttributeKey, "\"variant passed\"");
+        verify(span).addEvent("feature_flag", expectedAttr);
+    }
+
+    @Test
+    @DisplayName("should not call addEvent because there is no active span")
+    void should_not_call_add_event_when_no_active_span() {
+        HookContext<String> hookContext = HookContext.<String>builder()
+                .flagKey("test_key")
+                .type(FlagValueType.STRING)
+                .providerMetadata(() -> "test provider")
+                .ctx(new MutableContext())
+                .defaultValue("default")
+                .build();
+        FlagEvaluationDetails<String> details = FlagEvaluationDetails.<String>builder()
+                .variant(null)
+                .value("variant passed")
+                .build();
+        mockedSpan.when(Span::current).thenReturn(null);
+        openTelemetryHook.after(hookContext, details, null);
+        verifyNoInteractions(span);
+    }
+
+    @Test
+    @DisplayName("should record an exception in span during error method execution")
+    void should_record_exception_in_span_during_error_method_execution() {
+        RuntimeException runtimeException = new RuntimeException("could not resolve the flag");
+        mockedSpan.when(Span::current).thenReturn(span);
+        openTelemetryHook.error(hookContext, runtimeException, null);
+        Attributes expectedAttr = Attributes.of(flagKeyAttributeKey, "test_key",
+                providerNameAttributeKey, "test provider");
+        verify(span).recordException(runtimeException, expectedAttr);
+    }
+
+    @Test
+    @DisplayName("should not call recordException because there is no active span")
+    void should_not_call_record_exception_when_no_active_span() {
+        RuntimeException runtimeException = new RuntimeException("could not resolve the flag");
+        mockedSpan.when(Span::current).thenReturn(null);
+        openTelemetryHook.error(hookContext, runtimeException, null);
+        verifyNoInteractions(span);
+    }
+
 }
