55a2ac24260ee6ef1b909924ecf9694a07ad6969
Thiyagu GK <thiyagu103@gmail.com>
2022-12-22 22:44:44 +0530

    feat: add spec compliant otel hook (#169)
    feat: add spec compliant otel hook
    
    Signed-off-by: thiyagu06 <thiyagu103@gmail.com>

--- hooks/README.md
+++ hooks/README.md
@@ -1,3 +1,28 @@
 # OpenFeature Java Hooks
 
-Hooks are a mechanism whereby application developers can add arbitrary behavior to flag evaluation. They operate similarly to middleware in many web frameworks. Please see the [spec](https://github.com/open-feature/spec/blob/main/specification/sections/04-hooks.md) for more details.
+The OpenTelemetry hook for OpenFeature provides
+a [spec compliant] (https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/feature-flags.md)
+way to automatically add a feature flag
+evaluation to a span as a span event. This can be used to determine the impact a feature has on a request,
+enabling enhanced observability use cases, such as A/B testing or progressive feature releases.
+
+## Installation
+<!-- x-release-please-start-version -->
+```xml
+    <dependency>
+        <groupId>dev.openfeature.contrib.hooks</groupId>
+        <artifactId>otel</artifactId>
+        <version>0.4.0</version>
+    </dependency>
+```
+<!-- x-release-please-end-version -->
+
+## Usage
+
+OpenFeature provider various ways to register hooks. The location that a hook is registered affects when the hook is
+run. It's recommended to register the `OpenTelemetryHook` globally in most situations, but it's possible to only enable
+the hook on specific clients. You should **never** register the `OpenTelemetryHook` globally and on a client.
+
+```
+     OpenFeatureAPI.getInstance().addHooks(new OpenTelemetryHook());
+```
--- hooks/open-telemetry/src/main/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHook.java
+++ hooks/open-telemetry/src/main/java/dev/openfeature/contrib/hooks/otel/OpenTelemetryHook.java
@@ -1,29 +1,69 @@
 package dev.openfeature.contrib.hooks.otel;
 
-import dev.openfeature.sdk.Client;
-import dev.openfeature.sdk.NoOpProvider;
-import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.FlagEvaluationDetails;
+import dev.openfeature.sdk.Hook;
+import dev.openfeature.sdk.HookContext;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
 
-/** 
- * A placeholder.
+import java.util.Map;
+
+/**
+ * The OpenTelemetry hook  provides a way to automatically add a feature flag evaluation to a span as a span event.
+ * Refer to <a href="https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/feature-flags.md">OpenTelemetry</a>
  */
-public class OpenTelemetryHook {
-    
-    /** 
+public class OpenTelemetryHook implements Hook {
+
+    private static final String EVENT_NAME = "feature_flag";
+
+    private final AttributeKey<String> flagKeyAttributeKey = AttributeKey.stringKey(EVENT_NAME + ".flag_key");
+
+    private final AttributeKey<String> providerNameAttributeKey = AttributeKey.stringKey(EVENT_NAME + ".provider_name");
+
+    private final AttributeKey<String> variantAttributeKey = AttributeKey.stringKey(EVENT_NAME + ".variant");
+
+    /**
      * Create a new OpenTelemetryHook instance.
      */
     public OpenTelemetryHook() {
     }
 
-    /** 
-     * A test method...
+    /**
+     * Records the event in the current span after the successful flag evaluation.
      *
-     * @return {boolean}
+     * @param ctx     Information about the particular flag evaluation
+     * @param details Information about how the flag was resolved, including any resolved values.
+     * @param hints   An immutable mapping of data for users to communicate to the hooks.
      */
-    public static boolean test() {
-        OpenFeatureAPI.getInstance().setProvider(new NoOpProvider());
-        Client client = OpenFeatureAPI.getInstance().getClient();
-        return client.getBooleanValue("test2", true);
+    @Override
+    public void after(HookContext ctx, FlagEvaluationDetails details, Map hints) {
+        Span currentSpan = Span.current();
+        if (currentSpan != null) {
+            String variant = details.getVariant() != null ? details.getVariant() : String.valueOf(details.getValue());
+            Attributes attributes = Attributes.of(
+                    flagKeyAttributeKey, ctx.getFlagKey(),
+                    providerNameAttributeKey, ctx.getProviderMetadata().getName(),
+                    variantAttributeKey, variant);
+            currentSpan.addEvent(EVENT_NAME, attributes);
+        }
     }
 
+    /**
+     * Records the error details in the current span after the flag evaluation has processed abnormally.
+     *
+     * @param ctx   Information about the particular flag evaluation
+     * @param error The exception that was thrown.
+     * @param hints An immutable mapping of data for users to communicate to the hooks.
+     */
+    @Override
+    public void error(HookContext ctx, Exception error, Map hints) {
+        Span currentSpan = Span.current();
+        if (currentSpan != null) {
+            Attributes attributes = Attributes.of(
+                    flagKeyAttributeKey, ctx.getFlagKey(),
+                    providerNameAttributeKey, ctx.getProviderMetadata().getName());
+            currentSpan.recordException(error, attributes);
+        }
+    }
 }
