0dbee72ebf89b750b419d3814dc92ac952064d79
Todd Baert <toddbaert@gmail.com>
2023-02-06 09:00:27 -0500

    fix: make flagsmith classes public
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- providers/flagsmith/src/main/java/dev.openfeature.contrib.providers.flagsmith/FlagsmithProvider.java
+++ providers/flagsmith/src/main/java/dev.openfeature.contrib.providers.flagsmith/FlagsmithProvider.java
@@ -31,7 +31,7 @@ import lombok.extern.slf4j.Slf4j;
  * FlagsmithProvider is the JAVA provider implementation for the feature flag solution Flagsmith.
  */
 @Slf4j
-class FlagsmithProvider implements FeatureProvider {
+public class FlagsmithProvider implements FeatureProvider {
 
     private static final String NAME = "Flagsmith Provider";
     private static FlagsmithClient flagsmith;
