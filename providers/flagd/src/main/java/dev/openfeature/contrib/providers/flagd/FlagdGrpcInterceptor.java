9168797370e565b7ceb2b25ae4cb940ad8e0dcf3
Kavindu Dodanduwa <Kavindu-Dodan@users.noreply.github.com>
2023-04-25 13:21:51 -0700

    feat: flagd manual otel interceptor for grpc (#286)
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

---
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/FlagdGrpcInterceptor.java
@@ -0,0 +1,54 @@
+package dev.openfeature.contrib.providers.flagd;
+
+import io.grpc.CallOptions;
+import io.grpc.Channel;
+import io.grpc.ClientCall;
+import io.grpc.ClientInterceptor;
+import io.grpc.ForwardingClientCall;
+import io.grpc.Metadata;
+import io.grpc.MethodDescriptor;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.propagation.TextMapSetter;
+
+import javax.annotation.Nullable;
+
+/**
+ * FlagdGrpcInterceptor is an interceptor for grpc communication from java-sdk to flagd.
+ * <a href="https://github.com/open-telemetry/opentelemetry-java-docs">credits</a>
+ */
+final class FlagdGrpcInterceptor implements ClientInterceptor {
+    private static final TextMapSetter<Metadata> SETTER = new Setter();
+
+    private final OpenTelemetry openTelemetry;
+
+    FlagdGrpcInterceptor(final OpenTelemetry openTelemetry) {
+        this.openTelemetry = openTelemetry;
+    }
+
+    @Override public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> methodDescriptor,
+                                                                         CallOptions callOptions, Channel channel) {
+
+        final ClientCall<ReqT, RespT> call = channel.newCall(methodDescriptor, callOptions);
+
+        return new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(call) {
+            @Override public void start(Listener<RespT> responseListener, Metadata headers) {
+                openTelemetry.getPropagators().getTextMapPropagator().inject(Context.current(), headers, SETTER);
+                super.start(responseListener, headers);
+            }
+        };
+    }
+
+    /**
+     * Setter implements TextMapSetter with carrier check.
+     */
+    static class Setter implements TextMapSetter<Metadata> {
+        @Override public void set(@Nullable Metadata carrier, String key, String value) {
+            if (carrier == null) {
+                return;
+            }
+
+            carrier.put(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER), value);
+        }
+    }
+}
