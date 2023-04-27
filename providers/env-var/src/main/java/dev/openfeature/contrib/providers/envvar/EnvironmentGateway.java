3ee4db5b4a2a92b4ad1a7ee9b2968f52e9c68d1d
Kavindu Dodanduwa <Kavindu-Dodan@users.noreply.github.com>
2023-04-27 15:41:17 -0700

    Update providers/env-var/src/main/java/dev/openfeature/contrib/providers/envvar/EnvironmentGateway.java
    Co-authored-by: Todd Baert <toddbaert@gmail.com>
    Signed-off-by: Kavindu Dodanduwa <Kavindu-Dodan@users.noreply.github.com>

--- providers/env-var/src/main/java/dev/openfeature/contrib/providers/envvar/EnvironmentGateway.java
+++ providers/env-var/src/main/java/dev/openfeature/contrib/providers/envvar/EnvironmentGateway.java
@@ -1,7 +1,7 @@
 package dev.openfeature.contrib.providers.envvar;
 
 /**
- * This is an abstraction to fetch an environment variables. It can be used to support
+ * This is an abstraction to fetch environment variables. It can be used to support
  * environment-specific access or provide additional functionality, like prefixes,
  * casing and even sources like spring configurations which come from different sources.
  * Also, a test double could implement this interface, making the tests independent of the actual environment.
