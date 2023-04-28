8d9f8312f1c7cda0ed58360e567af59781dc9782
Kavindu Dodanduwa <Kavindu-Dodan@users.noreply.github.com>
2023-04-28 09:38:52 -0700

    chore: add more unit tests (#299)
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/FlagProviderBuilderTest.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/FlagProviderBuilderTest.java
@@ -1,6 +1,8 @@
 package dev.openfeature.contrib.providers.flagd;
 
+import io.opentelemetry.api.OpenTelemetry;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static dev.openfeature.contrib.providers.flagd.Config.DEFAULT_CACHE;
 import static dev.openfeature.contrib.providers.flagd.Config.DEFAULT_HOST;
@@ -10,6 +12,7 @@ import static dev.openfeature.contrib.providers.flagd.Config.DEFAULT_PORT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FlagProviderBuilderTest {
 
@@ -17,14 +20,39 @@ public class FlagProviderBuilderTest {
     public void TestDefaults() {
         final FlagdOptions builder = FlagdOptions.builder().build();
 
-        assertEquals(builder.getHost(),DEFAULT_HOST );
-        assertEquals(Integer.toString(builder.getPort()),DEFAULT_PORT );
+        assertEquals(builder.getHost(), DEFAULT_HOST);
+        assertEquals(Integer.toString(builder.getPort()), DEFAULT_PORT);
         assertFalse(builder.isTls());
         assertNull(builder.getCertPath());
         assertNull(builder.getSocketPath());
-        assertEquals(builder.getCacheType(),DEFAULT_CACHE );
+        assertEquals(builder.getCacheType(), DEFAULT_CACHE);
         assertEquals(builder.getMaxCacheSize(), DEFAULT_MAX_CACHE_SIZE);
         assertEquals(builder.getMaxEventStreamRetries(), DEFAULT_MAX_EVENT_STREAM_RETRIES);
         assertNull(builder.getOpenTelemetry());
+    }
+
+    @Test
+    public void TestBuilderOptions(){
+        OpenTelemetry openTelemetry = Mockito.mock(OpenTelemetry.class);
+
+        FlagdOptions flagdOptions = FlagdOptions.builder()
+                .host("https://hosted-flagd")
+                .port(80)
+                .tls(true)
+                .certPath("etc/cert/ca.crt")
+                .cacheType("lru")
+                .maxCacheSize(100)
+                .maxEventStreamRetries(1)
+                .openTelemetry(openTelemetry)
+                .build();
+
+        assertEquals(flagdOptions.getHost(), "https://hosted-flagd");
+        assertEquals(flagdOptions.getPort(), 80);
+        assertTrue(flagdOptions.isTls());
+        assertEquals(flagdOptions.getCertPath(), "etc/cert/ca.crt");
+        assertEquals(flagdOptions.getCacheType(), "lru");
+        assertEquals(flagdOptions.getMaxCacheSize(), 100);
+        assertEquals(flagdOptions.getMaxEventStreamRetries(), 1);
+        assertEquals(flagdOptions.getOpenTelemetry(), openTelemetry);
     }
 }
