5e6ab32c7b098f2f6d2d0b8110d388cbc3172d32
Kavindu Dodanduwa <Kavindu-Dodan@users.noreply.github.com>
2023-07-19 12:36:12 -0700

    fix: otel hook attribute naming correction (#359)
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- hooks/open-telemetry/src/main/java/dev/openfeature/contrib/hooks/otel/OTelCommons.java
+++ hooks/open-telemetry/src/main/java/dev/openfeature/contrib/hooks/otel/OTelCommons.java
@@ -1,14 +1,16 @@
 package dev.openfeature.contrib.hooks.otel;
 
 import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.semconv.trace.attributes.SemanticAttributes;
 
 class OTelCommons {
     // Define semantic conventions
     // Refer - https://opentelemetry.io/docs/specs/otel/logs/semantic_conventions/feature-flags/
     static final String EVENT_NAME = "feature_flag";
-    static final AttributeKey<String> flagKeyAttributeKey = AttributeKey.stringKey(EVENT_NAME + ".flag_key");
-    static final AttributeKey<String> providerNameAttributeKey = AttributeKey.stringKey(EVENT_NAME + ".provider_name");
-    static final AttributeKey<String> variantAttributeKey = AttributeKey.stringKey(EVENT_NAME + ".variant");
+
+    static final AttributeKey<String> flagKeyAttributeKey = SemanticAttributes.FEATURE_FLAG_KEY;
+    static final AttributeKey<String> providerNameAttributeKey = SemanticAttributes.FEATURE_FLAG_PROVIDER_NAME;
+    static final AttributeKey<String> variantAttributeKey = SemanticAttributes.FEATURE_FLAG_VARIANT;
 
     // Define non convention attribute keys
     static final String REASON_KEY = "reason";
