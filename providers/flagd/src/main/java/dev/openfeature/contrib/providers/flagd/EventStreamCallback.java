258ca10c815e61f782442fb3b8c87cd1dd79fe75
Giovanni Liva <giovanni.liva@dynatrace.com>
2023-07-27 15:13:13 +0200

    feat!: add events support (#361)
    Signed-off-by: Giovanni Liva <giovanni.liva@dynatrace.com>

--- providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/EventStreamCallback.java
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/EventStreamCallback.java
@@ -1,10 +1,16 @@
 package dev.openfeature.contrib.providers.flagd;
 
+import dev.openfeature.sdk.ProviderState;
+
 /**
  * Defines behaviour required of event stream callbacks.
  */
 interface EventStreamCallback {
-    void setEventStreamAlive(boolean alive);
+    void setState(ProviderState state);
 
     void restartEventStream() throws Exception;
+
+    void emitSuccessReconnectionEvents();
+
+    void emitConfigurationChangeEvent();
 }
