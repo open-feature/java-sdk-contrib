51bb787471e3a2d06d45c4148e2513a6f9965a37
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-08-17 15:34:18 -0700

    improve netty usage
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/grpc/GrpcConnectorTest.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/grpc/GrpcConnectorTest.java
@@ -124,80 +124,6 @@ public class GrpcConnectorTest {
     }
 
     @Test
-    void path_arg_should_build_domain_socket_with_correct_path() {
-        final String path = "/some/path";
-
-        ServiceGrpc.ServiceBlockingStub mockBlockingStub = mock(ServiceGrpc.ServiceBlockingStub.class);
-        ServiceGrpc.ServiceStub mockStub = mock(ServiceGrpc.ServiceStub.class);
-        NettyChannelBuilder mockChannelBuilder = getMockChannelBuilderSocket();
-
-        try (MockedStatic<ServiceGrpc> mockStaticService = mockStatic(ServiceGrpc.class)) {
-            mockStaticService.when(() -> ServiceGrpc.newBlockingStub(any(Channel.class)))
-                    .thenReturn(mockBlockingStub);
-            mockStaticService.when(() -> ServiceGrpc.newStub(any()))
-                    .thenReturn(mockStub);
-
-            try (MockedStatic<NettyChannelBuilder> mockStaticChannelBuilder = mockStatic(NettyChannelBuilder.class)) {
-
-                try (MockedConstruction<EpollEventLoopGroup> mockEpollEventLoopGroup = mockConstruction(
-                        EpollEventLoopGroup.class,
-                        (mock, context) -> {
-                        })) {
-                    when(NettyChannelBuilder.forAddress(any(DomainSocketAddress.class))).thenReturn(mockChannelBuilder);
-
-                    new GrpcConnector(FlagdOptions.builder().socketPath(path).build(), null, null);
-
-                    // verify path matches
-                    mockStaticChannelBuilder.verify(() -> NettyChannelBuilder
-                            .forAddress(argThat((DomainSocketAddress d) -> {
-                                assertEquals(d.path(), path); // path should match
-                                return true;
-                            })), times(1));
-                }
-            }
-        }
-    }
-
-    @Test
-    void no_args_socket_env_should_build_domain_socket_with_correct_path() throws Exception {
-        final String path = "/some/other/path";
-
-        new EnvironmentVariables("FLAGD_SOCKET_PATH", path).execute(() -> {
-
-            ServiceGrpc.ServiceBlockingStub mockBlockingStub = mock(ServiceGrpc.ServiceBlockingStub.class);
-            ServiceGrpc.ServiceStub mockStub = mock(ServiceGrpc.ServiceStub.class);
-            NettyChannelBuilder mockChannelBuilder = getMockChannelBuilderSocket();
-
-            try (MockedStatic<ServiceGrpc> mockStaticService = mockStatic(ServiceGrpc.class)) {
-                mockStaticService.when(() -> ServiceGrpc.newBlockingStub(any(Channel.class)))
-                        .thenReturn(mockBlockingStub);
-                mockStaticService.when(() -> ServiceGrpc.newStub(any()))
-                        .thenReturn(mockStub);
-
-                try (MockedStatic<NettyChannelBuilder> mockStaticChannelBuilder = mockStatic(
-                        NettyChannelBuilder.class)) {
-
-                    try (MockedConstruction<EpollEventLoopGroup> mockEpollEventLoopGroup = mockConstruction(
-                            EpollEventLoopGroup.class,
-                            (mock, context) -> {
-                            })) {
-                        mockStaticChannelBuilder.when(() -> NettyChannelBuilder
-                                .forAddress(any(DomainSocketAddress.class))).thenReturn(mockChannelBuilder);
-
-                        new GrpcConnector(FlagdOptions.builder().build(), null, null);
-
-                        //verify path matches & called times(= 1 as we rely on reusable channel)
-                        mockStaticChannelBuilder.verify(() -> NettyChannelBuilder
-                                .forAddress(argThat((DomainSocketAddress d) -> {
-                                    return d.path() == path;
-                                })), times(1));
-                    }
-                }
-            }
-        });
-    }
-
-    @Test
     void host_and_port_arg_should_build_tcp_socket() {
         final String host = "host.com";
         final int port = 1234;
@@ -225,6 +151,17 @@ public class GrpcConnectorTest {
                         .forAddress(host, port), times(1));
             }
         }
+    }
+
+    @Test
+    void should_fail_when_epoll_is_unavailable() {
+        // Manually override system property to make epoll unavailable
+        System.setProperty("io.netty.transport.noNative", "true");
+
+        final FlagdOptions flagdOptions = FlagdOptions.builder().socketPath("path").build();
+        assertThrows(RuntimeException.class, () -> new GrpcConnector(flagdOptions, null, null));
+
+        System.clearProperty("io.netty.transport.noNative");
     }
 
     @Test
