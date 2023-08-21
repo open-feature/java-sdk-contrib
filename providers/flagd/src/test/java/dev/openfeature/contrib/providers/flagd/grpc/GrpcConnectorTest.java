35d293bbea9d60b13bd82c9e8483388629fc4d46
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-08-21 10:01:19 -0700

    re-add test with conditional run annotations
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/grpc/GrpcConnectorTest.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/grpc/GrpcConnectorTest.java
@@ -7,9 +7,15 @@ import dev.openfeature.flagd.grpc.ServiceGrpc;
 import io.grpc.Channel;
 import io.grpc.netty.NettyChannelBuilder;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.unix.DomainSocketAddress;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedConstruction;
 import org.mockito.MockedStatic;
 import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
 
@@ -17,12 +23,15 @@ import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
@@ -174,6 +183,89 @@ public class GrpcConnectorTest {
                     // verify host/port matches & called times(= 1 as we rely on reusable channel)
                     mockStaticChannelBuilder.verify(() -> NettyChannelBuilder.
                             forAddress(host, port), times(1));
+                }
+            }
+        });
+    }
+
+
+    /**
+     * OS Specific test - This test is valid only on Linux system as it rely on epoll availability
+    * */
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void path_arg_should_build_domain_socket_with_correct_path() {
+        final String path = "/some/path";
+
+        ServiceGrpc.ServiceBlockingStub mockBlockingStub = mock(ServiceGrpc.ServiceBlockingStub.class);
+        ServiceGrpc.ServiceStub mockStub = mock(ServiceGrpc.ServiceStub.class);
+        NettyChannelBuilder mockChannelBuilder = getMockChannelBuilderSocket();
+
+        try (MockedStatic<ServiceGrpc> mockStaticService = mockStatic(ServiceGrpc.class)) {
+            mockStaticService.when(() -> ServiceGrpc.newBlockingStub(any(Channel.class)))
+                    .thenReturn(mockBlockingStub);
+            mockStaticService.when(() -> ServiceGrpc.newStub(any()))
+                    .thenReturn(mockStub);
+
+            try (MockedStatic<NettyChannelBuilder> mockStaticChannelBuilder = mockStatic(NettyChannelBuilder.class)) {
+
+                try (MockedConstruction<EpollEventLoopGroup> mockEpollEventLoopGroup = mockConstruction(
+                        EpollEventLoopGroup.class,
+                        (mock, context) -> {
+                        })) {
+                    when(NettyChannelBuilder.forAddress(any(DomainSocketAddress.class))).thenReturn(mockChannelBuilder);
+
+                    new GrpcConnector(FlagdOptions.builder().socketPath(path).build(), null, null);
+
+                    // verify path matches
+                    mockStaticChannelBuilder.verify(() -> NettyChannelBuilder
+                            .forAddress(argThat((DomainSocketAddress d) -> {
+                                assertEquals(d.path(), path); // path should match
+                                return true;
+                            })), times(1));
+                }
+            }
+        }
+    }
+
+    /**
+     * OS Specific test - This test is valid only on Linux system as it rely on epoll availability
+     * */
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void no_args_socket_env_should_build_domain_socket_with_correct_path() throws Exception {
+        final String path = "/some/other/path";
+
+        new EnvironmentVariables("FLAGD_SOCKET_PATH", path).execute(() -> {
+
+            ServiceGrpc.ServiceBlockingStub mockBlockingStub = mock(ServiceGrpc.ServiceBlockingStub.class);
+            ServiceGrpc.ServiceStub mockStub = mock(ServiceGrpc.ServiceStub.class);
+            NettyChannelBuilder mockChannelBuilder = getMockChannelBuilderSocket();
+
+            try (MockedStatic<ServiceGrpc> mockStaticService = mockStatic(ServiceGrpc.class)) {
+                mockStaticService.when(() -> ServiceGrpc.newBlockingStub(any(Channel.class)))
+                        .thenReturn(mockBlockingStub);
+                mockStaticService.when(() -> ServiceGrpc.newStub(any()))
+                        .thenReturn(mockStub);
+
+                try (MockedStatic<NettyChannelBuilder> mockStaticChannelBuilder = mockStatic(
+                        NettyChannelBuilder.class)) {
+
+                    try (MockedConstruction<EpollEventLoopGroup> mockEpollEventLoopGroup = mockConstruction(
+                            EpollEventLoopGroup.class,
+                            (mock, context) -> {
+                            })) {
+                        mockStaticChannelBuilder.when(() -> NettyChannelBuilder
+                                .forAddress(any(DomainSocketAddress.class))).thenReturn(mockChannelBuilder);
+
+                        new GrpcConnector(FlagdOptions.builder().build(), null, null);
+
+                        //verify path matches & called times(= 1 as we rely on reusable channel)
+                        mockStaticChannelBuilder.verify(() -> NettyChannelBuilder
+                                .forAddress(argThat((DomainSocketAddress d) -> {
+                                    return d.path() == path;
+                                })), times(1));
+                    }
                 }
             }
         });
