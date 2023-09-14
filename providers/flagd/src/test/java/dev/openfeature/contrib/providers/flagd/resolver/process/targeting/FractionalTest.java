ba135f0af6114fb97c7e680213cd29da2fadd531
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-09-14 11:37:21 -0700

    fix tests
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/resolver/process/targeting/FractionalTest.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/resolver/process/targeting/FractionalTest.java
@@ -55,7 +55,7 @@ class FractionalTest {
         Object evaluate = fractional.evaluate(rule, data);
 
         // then
-        assertEquals("green", evaluate);
+        assertEquals("red", evaluate);
     }
 
     @Test
@@ -98,7 +98,7 @@ class FractionalTest {
         Object evaluate = fractional.evaluate(rule, data);
 
         // then
-        assertEquals("red", evaluate);
+        assertEquals("green", evaluate);
     }
 
     @Test
