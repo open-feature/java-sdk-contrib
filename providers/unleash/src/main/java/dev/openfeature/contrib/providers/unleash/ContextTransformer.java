ec6b3c912c20cb3c4e435278a4967215278d8456
Liran M <77168114+liran2000@users.noreply.github.com>
2023-09-28 04:25:59 +0300

    feat: add Unleash provider (#424)
    Signed-off-by: liran2000 <liran2000@gmail.com>
    Co-authored-by: Todd Baert <todd.baert@dynatrace.com>
    Co-authored-by: Ivar Conradi Østhus <ivarconr@gmail.com>

---
+++ providers/unleash/src/main/java/dev/openfeature/contrib/providers/unleash/ContextTransformer.java
@@ -0,0 +1,50 @@
+package dev.openfeature.contrib.providers.unleash;
+
+import dev.openfeature.sdk.EvaluationContext;
+import io.getunleash.UnleashContext;
+
+import java.time.ZonedDateTime;
+
+/**
+ * Transformer from Unleash context to OpenFeature context and vice versa.
+ */
+public class ContextTransformer {
+
+    public static final String CONTEXT_APP_NAME = "appName";
+    public static final String CONTEXT_USER_ID = "userId";
+    public static final String CONTEXT_ENVIRONMENT = "environment";
+    public static final String CONTEXT_REMOTE_ADDRESS = "remoteAddress";
+    public static final String CONTEXT_SESSION_ID = "sessionId";
+    public static final String CONTEXT_CURRENT_TIME = "currentTime";
+
+    protected static UnleashContext transform(EvaluationContext ctx) {
+        UnleashContext.Builder unleashContextBuilder = new UnleashContext.Builder();
+        ctx.asObjectMap().forEach((k, v) -> {
+            switch (k) {
+                case CONTEXT_APP_NAME:
+                    unleashContextBuilder.appName(String.valueOf(v));
+                    break;
+                case CONTEXT_USER_ID:
+                    unleashContextBuilder.userId(String.valueOf(v));
+                    break;
+                case CONTEXT_ENVIRONMENT:
+                    unleashContextBuilder.environment(String.valueOf(v));
+                    break;
+                case CONTEXT_REMOTE_ADDRESS:
+                    unleashContextBuilder.remoteAddress(String.valueOf(v));
+                    break;
+                case CONTEXT_SESSION_ID:
+                    unleashContextBuilder.sessionId(String.valueOf(v));
+                    break;
+                case CONTEXT_CURRENT_TIME:
+                    unleashContextBuilder.currentTime(ZonedDateTime.parse(String.valueOf(v)));
+                    break;
+                default:
+                    unleashContextBuilder.addProperty(k, String.valueOf(v));
+                    break;
+            }
+        });
+        return unleashContextBuilder.build();
+    }
+
+}
