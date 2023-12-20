66bf5e32ded14df2e904c5059d3bb086d9838747
Todd Baert <todd.baert@dynatrace.com>
2023-12-20 13:20:10 -0500

    fixup: improve test stability
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/e2e/reconnect/steps/StepDefinitions.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/e2e/reconnect/steps/StepDefinitions.java
@@ -1,6 +1,7 @@
 package dev.openfeature.contrib.providers.flagd.e2e.reconnect.steps;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.Duration;
 import java.util.function.Consumer;
@@ -20,7 +21,7 @@ import io.cucumber.java.en.When;
 
 /**
  * Test suite for testing flagd provider reconnect functionality.
- * The associated container run a flagd instance which restarts every 5s. 
+ * The associated container run a flagd instance which restarts every 5s.
  */
 @Isolated()
 @Order(value = Integer.MAX_VALUE)
@@ -52,8 +53,6 @@ public class StepDefinitions {
 
     public StepDefinitions() {
         StepDefinitions.client = OpenFeatureAPI.getInstance().getClient("unstable");
-        client.onProviderReady(this.readyHandler);
-        client.onProviderError(this.errorHandler);
         OpenFeatureAPI.getInstance().setProviderAndWait("unstable", provider);
     }
 
@@ -71,30 +70,37 @@ public class StepDefinitions {
 
     @When("a PROVIDER_READY handler and a PROVIDER_ERROR handler are added")
     public void a_provider_ready_handler_and_a_provider_error_handler_are_added() {
-        // done in constructor
+        client.onProviderReady(this.readyHandler);
+        client.onProviderError(this.errorHandler);
     }
 
     @Then("the PROVIDER_READY handler must run when the provider connects")
     public void the_provider_ready_handler_must_run_when_the_provider_connects() {
-         // should already be at 1 from initial connection
-        assertEquals(1, readyHandlerRunCount);
+        // no errors expected yet
+        assertEquals(0, errorHandlerRunCount);
+        // wait up to 15 seconds for a connect (PROVIDER_READY event)
+        Awaitility.await().atMost(Duration.ofSeconds(15))
+                .until(() -> {
+                    return this.readyHandlerRunCount == 1;
+                });
+
     }
 
     @Then("the PROVIDER_ERROR handler must run when the provider's connection is lost")
     public void the_provider_error_handler_must_run_when_the_provider_s_connection_is_lost() {
         // wait up to 15 seconds for a disconnect (PROVIDER_ERROR event)
         Awaitility.await().atMost(Duration.ofSeconds(15))
-        .until(() -> {
-            return this.errorHandlerRunCount > 0;
-        });
+                .until(() -> {
+                    return this.errorHandlerRunCount > 0;
+                });
     }
 
     @Then("when the connection is reestablished the PROVIDER_READY handler must run again")
     public void when_the_connection_is_reestablished_the_provider_ready_handler_must_run_again() {
         // wait up to 15 seconds for a reconnect (PROVIDER_READY event)
         Awaitility.await().atMost(Duration.ofSeconds(15))
-        .until(() -> {
-            return this.readyHandlerRunCount > 1;
-        });
+                .until(() -> {
+                    return this.readyHandlerRunCount > 1;
+                });
     }
 }
