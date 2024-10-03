27543c9edab269fdafe197e9b528f81525145ff7
Guido Breitenhuber <157148191+guidobrei@users.noreply.github.com>
2024-10-03 18:10:47 +0200

    build(flagd): OS agnostic flagd provider build (#998)
    Signed-off-by: Guido Breitenhuber <guido.breitenhuber@dynatrace.com>
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>
    Co-authored-by: Todd Baert <todd.baert@dynatrace.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/resolver/process/TestUtils.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/resolver/process/TestUtils.java
@@ -3,9 +3,12 @@ package dev.openfeature.contrib.providers.flagd.resolver.process;
 import dev.openfeature.contrib.providers.flagd.resolver.process.model.FlagParser;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 public class TestUtils {
     public static final String VALID_SIMPLE = "flagConfigurations/valid-simple.json";
@@ -15,21 +18,23 @@ public class TestUtils {
     public static final String INVALID_CFG = "flagConfigurations/invalid-configuration.json";
     public static final String UPDATABLE_FILE = "flagConfigurations/updatableFlags.json";
 
-
     public static String getFlagsFromResource(final String file) throws IOException {
-        final URL url = FlagParser.class.getClassLoader().getResource(file);
-        if (url == null) {
-            throw new IllegalStateException(String.format("Resource %s not found", file));
-        } else {
-            return new String(Files.readAllBytes(Paths.get(url.getPath())));
-        }
+        final Path resourcePath = getResourcePathInternal(file);
+        return new String(Files.readAllBytes(resourcePath));
     }
 
     public static String getResourcePath(final String relativePath) {
-        final URL url = FlagParser.class.getClassLoader().getResource(relativePath);
-        if (url == null) {
-            throw new IllegalStateException(String.format("Resource %s not found", relativePath));
+        return getResourcePathInternal(relativePath).toString();
+    }
+
+    private static Path getResourcePathInternal(String file) {
+        try {
+            URL url = Objects.requireNonNull(FlagParser.class.getClassLoader().getResource(file));
+            return Paths.get(url.toURI());
+        } catch (NullPointerException e) {
+            throw new IllegalStateException(String.format("Resource %s not found", file), e);
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException("Invalid resource path", e);
         }
-        return url.getPath();
     }
 }
