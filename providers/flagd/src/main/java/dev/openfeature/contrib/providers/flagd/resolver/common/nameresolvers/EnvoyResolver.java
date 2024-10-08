0da27d89fa88993cbe42a5a158d5a19ee22565eb
Pradeep <pradeepbbl@gmail.com>
2024-10-08 02:21:42 +0200

    feat: added custom grpc resolver
    added custom gRPC resolver to support envoy proxy
    
    Signed-off-by: Pradeep Mishra <pradeepbbl@gmail.com>
    Signed-off-by: Pradeep <pradeepbbl@gmail.com>

---
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/common/nameresolvers/EnvoyResolver.java
@@ -0,0 +1,69 @@
+package dev.openfeature.contrib.providers.flagd.resolver.common.nameresolvers;
+
+import io.grpc.EquivalentAddressGroup;
+import io.grpc.NameResolver;
+import java.net.InetSocketAddress;
+import io.grpc.Attributes;
+import io.grpc.Status;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Envoy NameResolver, will always override the authority with the specified authority and
+ * use the socketAddress to connect.
+ * <p>
+ * Custom URI Scheme:
+ * <p>
+ *     envoy://[proxy-agent-host]:[proxy-agent-port]/[service-name]
+ * <p>
+ * `service-name` is used as authority instead host
+ */
+public class EnvoyResolver extends NameResolver {
+    private final URI uri;
+    private final String authority;
+    private Listener2 listener;
+
+    public EnvoyResolver(URI targetUri) {
+        this.uri = targetUri;
+        this.authority = targetUri.getPath().substring(1);
+    }
+
+    @Override
+    public String getServiceAuthority() {
+        return authority;
+    }
+
+    @Override
+    public void shutdown() {
+    }
+
+    @Override
+    public void start(Listener2 listener) {
+        this.listener = listener;
+        this.resolve();
+    }
+
+    @Override
+    public void refresh() {
+        this.resolve();
+    }
+
+    private void resolve() {
+        try {
+            InetSocketAddress address = new InetSocketAddress(this.uri.getHost(), this.uri.getPort());
+            Attributes addressGroupAttributes = Attributes.newBuilder()
+                    .set(EquivalentAddressGroup.ATTR_AUTHORITY_OVERRIDE, this.authority)
+                    .build();
+            List<EquivalentAddressGroup> equivalentAddressGroup = Collections.singletonList(
+                    new EquivalentAddressGroup(address, addressGroupAttributes)
+            );
+            ResolutionResult resolutionResult = ResolutionResult.newBuilder()
+                    .setAddresses(equivalentAddressGroup)
+                    .build();
+            this.listener.onResult(resolutionResult);
+        } catch (Exception e) {
+            this.listener.onError(Status.UNAVAILABLE.withDescription("Unable to resolve host ").withCause(e));
+        }
+    }
+}
---
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/common/nameresolvers/EnvoyResolverProvider.java
@@ -0,0 +1,44 @@
+package dev.openfeature.contrib.providers.flagd.resolver.common.nameresolvers;
+
+import io.grpc.NameResolver;
+import io.grpc.NameResolverProvider;
+import java.net.URI;
+
+public class EnvoyResolverProvider extends NameResolverProvider {
+    static final String ENVOY_SCHEME = "envoy";
+    static final String DEFAULT_SERVICE_NAME = "undefined";
+    @Override
+    protected boolean isAvailable() {
+        return true;
+    }
+
+    @Override
+    protected int priority() {
+        return 6;
+    }
+
+    @Override
+    public NameResolver newNameResolver(URI targetUri, NameResolver.Args args) {
+        if (!ENVOY_SCHEME.equals(targetUri.getScheme())) {
+            return null;
+        }
+
+        if (!isValidPath(targetUri.getPath()) || targetUri.getHost() == null || targetUri.getPort() == -1 ) {
+            throw new IllegalArgumentException("Incorrectly formatted target uri; "
+                    + "expected: '" + ENVOY_SCHEME + ":[//]<proxy-agent-host>:<proxy-agent-port>/<service-name>';"
+                    + "but was '" + targetUri + "'");
+        }
+
+        return new EnvoyResolver(targetUri);
+    }
+
+    @Override
+    public String getDefaultScheme() {
+        return ENVOY_SCHEME;
+    }
+
+    private static boolean isValidPath(String path) {
+        return !path.isEmpty() && !path.substring(1).isEmpty()
+                && !path.substring(1).contains("/");
+    }
+}
