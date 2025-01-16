853727fad2ee9414f515e7831b3ef757129756a5
Simon Schrottner <simon.schrottner@dynatrace.com>
2025-01-16 13:13:33 +0100

    Update providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/storage/connector/grpc/GrpcStreamConnector.java
    Co-authored-by: warber <72415058+warber@users.noreply.github.com>
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

--- providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/storage/connector/grpc/GrpcStreamConnector.java
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/storage/connector/grpc/GrpcStreamConnector.java
@@ -121,7 +121,6 @@ public class GrpcStreamConnector implements Connector {
                     metadataException = e;
                 }
 
-                log.info("stream");
                 while (!shutdown.get()) {
                     final GrpcResponseModel response = streamReceiver.take();
                     if (response.isComplete()) {
