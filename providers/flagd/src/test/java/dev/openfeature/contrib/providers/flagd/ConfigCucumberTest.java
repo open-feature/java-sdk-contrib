97bdaba87f9743f53c6f5e85ee8fb49f417b5ebb
christian.lutnik <christian.lutnik@dynatrace.com>
2025-02-18 08:37:07 +0100

    revert cucumber changes
    Signed-off-by: christian.lutnik <christian.lutnik@dynatrace.com>

--- providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/ConfigCucumberTest.java
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/ConfigCucumberTest.java
@@ -6,7 +6,7 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import org.junit.jupiter.api.Order;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectDirectories;
+import org.junit.platform.suite.api.SelectFile;
 import org.junit.platform.suite.api.Suite;
 
 /**
@@ -16,8 +16,7 @@ import org.junit.platform.suite.api.Suite;
 @Order(value = Integer.MAX_VALUE)
 @Suite
 @IncludeEngines("cucumber")
-// @SelectFile("test-harness/gherkin/config.feature")
-@SelectDirectories("test-harness/gherkin")
+@SelectFile("test-harness/gherkin/config.feature")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "dev.openfeature.contrib.providers.flagd.e2e.steps.config")
 public class ConfigCucumberTest {}
