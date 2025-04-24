8f03143ba466aae5fcd290bf11cdac2fb8af18b9
liran2000 <liran2000@gmail.com>
2025-04-24 17:45:51 +0300

    move to tool - draft - cont.
    Signed-off-by: liran2000 <liran2000@gmail.com>

--- providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/storage/connector/sync/http/PayloadCache.java
+++
@@ -1,6 +0,0 @@
-package dev.openfeature.contrib.providers.flagd.resolver.process.storage.connector.sync.http;
-
-public interface PayloadCache {
-    public void put(String payload);
-    public String get();
-}
