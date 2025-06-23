7650bbf212c0593fa81c31e3b68fe313643aa05a
Rahul Baradol <rahul.baradol.14@gmail.com>
2025-06-23 22:40:26 +0530

    refactor: making ofrep request dto immutable
    Signed-off-by: Rahul Baradol <rahul.baradol.14@gmail.com>

--- providers/ofrep/src/main/java/dev/openfeature/contrib/providers/ofrep/internal/OfrepRequest.java
+++ providers/ofrep/src/main/java/dev/openfeature/contrib/providers/ofrep/internal/OfrepRequest.java
@@ -1,12 +1,12 @@
 package dev.openfeature.contrib.providers.ofrep.internal;
 
 import com.google.common.collect.ImmutableMap;
-import lombok.Data;
+import lombok.Value;
 
 /**
  * Represents the request body for the OFREP API request.
  */
-@Data
+@Value
 public class OfrepRequest {
-    private final ImmutableMap<String, Object> context;
+    ImmutableMap<String, Object> context;
 }
