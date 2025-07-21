f4cdae797d852a80ca7f6602f14063b9c691b8cc
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-07-21 19:22:29 +0200

    upgrade version wasm
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- providers/go-feature-flag/download-wasm.sh
+++ providers/go-feature-flag/download-wasm.sh
@@ -2,6 +2,10 @@
 
 # This script downloads the wasm file from the go-feature-flag repository and adds it to the build.
 
-wasm_version="v1.45.0" # {{wasm_version}}
+wasm_version="v1.45.4" # {{wasm_version}}
 pwd
+echo "Downloading go-feature-flag wasm version: $wasm_version"
+ls "$(pwd)/wasm-releases"
+echo "Downloading go-feature-flag wasm evaluation version: $wasm_version"
+ls "$(pwd)/wasm-releases/evaluation"
 mv ./wasm-releases/evaluation/gofeatureflag-evaluation_$wasm_version.wasi ./src/main/resources/wasm/gofeatureflag-evaluation_$wasm_version.wasi
