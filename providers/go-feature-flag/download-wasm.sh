115b12ec82431bd87f3f6a00c7bf0e70e9a06a22
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-07-22 09:58:51 +0200

    remove unused part of the script
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- providers/go-feature-flag/download-wasm.sh
+++ providers/go-feature-flag/download-wasm.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# This script downloads the wasm file from the go-feature-flag repository and adds it to the build.
-
+# This script copy the wasm module from the git submodule repository and adds it to the build.
 wasm_version="v1.45.4" # {{wasm_version}}
-pwd
-echo "Downloading go-feature-flag wasm version: $wasm_version"
-ls "$(pwd)/wasm-releases"
-echo "Downloading go-feature-flag wasm evaluation version: $wasm_version"
-ls "$(pwd)/wasm-releases/evaluation"
 mv ./wasm-releases/evaluation/gofeatureflag-evaluation_$wasm_version.wasi ./src/main/resources/wasm/gofeatureflag-evaluation_$wasm_version.wasi
