9ec51933536941b8768988aa32c6b0b80609824d
jarebudev <23311805+jarebudev@users.noreply.github.com>
2025-11-11 23:04:09 +0000

    flagd provider creates named daemon threads for error executor
    Signed-off-by: jarebudev <23311805+jarebudev@users.noreply.github.com>

---
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/FlagdThreadFactory.java
@@ -0,0 +1,30 @@
+package dev.openfeature.contrib.providers.flagd;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Thread factory for the Flagd provider to allow named daemon threads to be created.
+ */
+public final class FlagdThreadFactory implements ThreadFactory {
+
+    private final AtomicInteger counter = new AtomicInteger();
+    private final String namePrefix;
+
+    /**
+     * {@link FlagdThreadFactory}'s constructor.
+     *
+     * @param namePrefix    Prefix used for setting the new thread's name.
+     */
+    FlagdThreadFactory(String namePrefix) {
+        this.namePrefix = namePrefix;
+    }
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        final Thread thread = new Thread(runnable);
+        thread.setDaemon(true);
+        thread.setName(namePrefix + "-" + counter.incrementAndGet());
+        return thread;
+    }
+}
