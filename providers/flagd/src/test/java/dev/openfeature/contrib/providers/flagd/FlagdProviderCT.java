48cd0f9c5a4be9eafdd587e95eb7ba794d98d91a
christian.lutnik <christian.lutnik@dynatrace.com>
2025-12-02 16:47:11 +0100

    add vmlens
    Signed-off-by: christian.lutnik <christian.lutnik@dynatrace.com>

---
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/FlagdProviderCT.java
@@ -0,0 +1,86 @@
+package dev.openfeature.contrib.providers.flagd;
+
+import com.vmlens.api.AllInterleavings;
+import com.vmlens.api.Runner;
+import dev.openfeature.contrib.providers.flagd.resolver.process.model.FeatureFlag;
+import dev.openfeature.sdk.ImmutableContext;
+import dev.openfeature.sdk.OpenFeatureAPI;
+import dev.openfeature.sdk.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FlagdProviderCT {
+    private FlagdProvider provider;
+
+    @BeforeEach
+    void setup() throws Exception {
+        provider = FlagdTestUtils.createInProcessProvider(
+                Map.of(
+                        "flag",
+                        new FeatureFlag(
+                                "ENABLED",
+                                "default",
+                                Map.of("default", "a", "other", "b"),
+                                "{\n"
+                                        + "        \"if\": [\n"
+                                        + "          {\n"
+                                        + "            \"ends_with\": [\n"
+                                        + "              {\n"
+                                        + "                \"var\": \"email\"\n"
+                                        + "              },\n"
+                                        + "              \"@ingen.com\"\n"
+                                        + "            ]\n"
+                                        + "          },\n"
+                                        + "          \"default\",\n"
+                                        + "          \"other\"\n"
+                                        + "        ]\n"
+                                        + "      }",
+                                null
+                        )
+                )
+        );
+        provider.initialize(ImmutableContext.EMPTY);
+    }
+/*
+    @Test
+    void concurrentFlagEvaluationsWork() {
+        var invocationContext = ImmutableContext.EMPTY;
+
+        try (var interleavings = new AllInterleavings("Concurrent Flag evaluations")) {
+            while (interleavings.hasNext()) {
+                Runner.runParallel(
+                        () -> assertEquals("a",
+                                provider.getStringEvaluation("flag", "z", invocationContext).getValue()),
+                        () -> assertEquals("a",
+                                provider.getStringEvaluation("flag", "z", invocationContext).getValue())
+                );
+            }
+        }
+    }*/
+
+    @Test
+    void flagEvaluationsWhileSettingContextWork() {
+        var invocationContext = ImmutableContext.EMPTY;
+
+        OpenFeatureAPI.getInstance().setProviderAndWait(provider);
+        var client = OpenFeatureAPI.getInstance().getClient();
+
+        var context = new ImmutableContext(Map.of("email", new Value("someone@ingen.com")));
+
+        try (var interleavings = new AllInterleavings("Concurrently setting client context and evaluating a Flag")) {
+            while (interleavings.hasNext()) {
+                Runner.runParallel(
+                        () -> assertTrue(List.of("a", "b")
+                                .contains(provider.getStringEvaluation("flag", "z", invocationContext).getValue())),
+                        () -> client.setEvaluationContext(context)
+                );
+            }
+        }
+    }
+}
---
+++ providers/flagd/src/test/java/dev/openfeature/contrib/providers/flagd/FlagdTestUtils.java
@@ -0,0 +1,88 @@
+package dev.openfeature.contrib.providers.flagd;
+
+import dev.openfeature.contrib.providers.flagd.resolver.Resolver;
+import dev.openfeature.contrib.providers.flagd.resolver.common.ChannelConnector;
+import dev.openfeature.contrib.providers.flagd.resolver.process.InProcessResolver;
+import dev.openfeature.contrib.providers.flagd.resolver.process.MockStorage;
+import dev.openfeature.contrib.providers.flagd.resolver.process.model.FeatureFlag;
+import dev.openfeature.contrib.providers.flagd.resolver.process.storage.StorageState;
+import dev.openfeature.contrib.providers.flagd.resolver.process.storage.StorageStateChange;
+import dev.openfeature.contrib.providers.flagd.resolver.rpc.RpcResolver;
+import dev.openfeature.contrib.providers.flagd.resolver.rpc.cache.Cache;
+import dev.openfeature.flagd.grpc.evaluation.ServiceGrpc;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.mockito.Mockito.mock;
+
+class FlagdTestUtils {
+    // test helper
+    // create provider with given grpc provider and state supplier
+    static FlagdProvider createProvider(ChannelConnector connector, ServiceGrpc.ServiceBlockingStub mockBlockingStub) {
+        final Cache cache = new Cache("lru", 5);
+        final ServiceGrpc.ServiceStub mockStub = mock(ServiceGrpc.ServiceStub.class);
+
+        return createProvider(connector, cache, mockStub, mockBlockingStub);
+    }
+
+    // create provider with given grpc provider, cache and state supplier
+    static FlagdProvider createProvider(
+            ChannelConnector connector, Cache cache, ServiceGrpc.ServiceStub mockStub,
+            ServiceGrpc.ServiceBlockingStub mockBlockingStub) {
+        final FlagdOptions flagdOptions = FlagdOptions.builder().build();
+        final RpcResolver grpcResolver = new RpcResolver(flagdOptions, cache, (connectionEvent) -> {});
+
+        try {
+            Field resolver = RpcResolver.class.getDeclaredField("connector");
+            resolver.setAccessible(true);
+            resolver.set(grpcResolver, connector);
+
+            Field stub = RpcResolver.class.getDeclaredField("stub");
+            stub.setAccessible(true);
+            stub.set(grpcResolver, mockStub);
+
+            Field blockingStub = RpcResolver.class.getDeclaredField("blockingStub");
+            blockingStub.setAccessible(true);
+            blockingStub.set(grpcResolver, mockBlockingStub);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        final FlagdProvider provider = new FlagdProvider(grpcResolver, true);
+        return provider;
+    }
+
+    static FlagdProvider createInProcessProvider(Map<String, FeatureFlag> mockFlags) {
+        final FlagdOptions flagdOptions = FlagdOptions.builder()
+                .resolverType(Config.Resolver.IN_PROCESS)
+                .offlineFlagSourcePath("") // this is new
+                .deadline(1000)
+                .build();
+        final FlagdProvider provider = new FlagdProvider(flagdOptions);
+        final MockStorage mockStorage = new MockStorage(
+                mockFlags,
+                new LinkedBlockingQueue<>(Arrays.asList(new StorageStateChange(StorageState.OK))));
+
+        try {
+            final Field flagResolver = FlagdProvider.class.getDeclaredField("flagResolver");
+            flagResolver.setAccessible(true);
+            final Resolver resolver = (Resolver) flagResolver.get(provider);
+
+            final Field flagStore = InProcessResolver.class.getDeclaredField("flagStore");
+            flagStore.setAccessible(true);
+            flagStore.set(resolver, mockStorage);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        return provider;
+    }
+
+    static FlagdProvider createInProcessProvider() {
+        return createInProcessProvider(Collections.emptyMap());
+    }
+}
