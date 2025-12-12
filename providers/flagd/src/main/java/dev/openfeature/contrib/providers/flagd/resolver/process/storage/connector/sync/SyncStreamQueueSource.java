f1b57c61464ad544f1f4bfc271ab8a64d7803daa
Todd Baert <todd.baert@dynatrace.com>
2025-12-12 12:13:03 -0500

    Apply suggestion from @toddbaert
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/storage/connector/sync/SyncStreamQueueSource.java
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/storage/connector/sync/SyncStreamQueueSource.java
@@ -123,7 +123,7 @@ public class SyncStreamQueueSource implements QueueSource {
             return;
         }
 
-        log.info("Reinitializing channel gRPC components in attempt to restore stream...");
+        log.info("Reinitializing channel gRPC components in attempt to restore stream.");
         GrpcComponents oldComponents = grpcComponents;
 
         try {
