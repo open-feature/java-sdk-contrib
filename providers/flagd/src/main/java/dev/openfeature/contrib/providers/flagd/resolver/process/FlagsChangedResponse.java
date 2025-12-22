728ec371380e3243cf28b85c3f52c9ce3c933400
Simon Schrottner <simon.schrottner@dynatrace.com>
2025-12-22 20:07:15 +0100

    feat: wasmanize
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

---
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/process/FlagsChangedResponse.java
@@ -0,0 +1,47 @@
+package dev.openfeature.contrib.providers.flagd.resolver.process;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public class FlagsChangedResponse {
+
+    @JsonProperty("success")
+    private boolean success;
+
+    @JsonProperty("changedFlags")
+    private List<String> changedFlags;
+
+    // Constructors
+    public FlagsChangedResponse() {
+    }
+
+    public FlagsChangedResponse(boolean success, List<String> changedFlags) {
+        this.success = success;
+        this.changedFlags = changedFlags;
+    }
+
+    // Getters and Setters
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public List<String> getChangedFlags() {
+        return changedFlags;
+    }
+
+    public void setChangedFlags(List<String> changedFlags) {
+        this.changedFlags = changedFlags;
+    }
+
+    @Override
+    public String toString() {
+        return "FlagChangeResponse{" +
+                "success=" + success +
+                ", changedFlags=" + changedFlags +
+                '}';
+    }
+}
