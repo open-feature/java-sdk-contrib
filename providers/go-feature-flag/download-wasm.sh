37e35ae1c7747f0d77bbe6e1d887c6d5024cbbbf
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2026-01-22 10:37:09 +0100

    chore(go-feature-flag): Remove unused script
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- providers/go-feature-flag/download-wasm.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# This script copy the wasm module from the git submodule repository and adds it to the build.
-wasm_version="v1.45.4" # {{wasm_version}}
-cp ./wasm-releases/evaluation/gofeatureflag-evaluation_$wasm_version.wasi ./src/main/resources/wasm/gofeatureflag-evaluation.wasi
