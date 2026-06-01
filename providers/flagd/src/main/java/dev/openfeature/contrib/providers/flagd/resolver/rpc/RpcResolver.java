9028e8312e409277e3173471fd7c569941c2965e
Todd Baert <todd.baert@dynatrace.com>
2026-06-01 13:57:27 -0400

    fixup: add variant check
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/rpc/RpcResolver.java
+++ providers/flagd/src/main/java/dev/openfeature/contrib/providers/flagd/resolver/rpc/RpcResolver.java
@@ -252,9 +252,7 @@ public final class RpcResolver implements Resolver {
         final String variant = getField(response, Config.VARIANT_FIELD);
 
         final ProviderEvaluation.ProviderEvaluationBuilder<ValT> resultBuilder;
-        if ("DEFAULT".equals(reason) && variant.isEmpty()) {
-            resultBuilder = ProviderEvaluation.<ValT>builder().value(defaultValue);
-        } else if ("DISABLED".equals(reason)) {
+        if (variant.isEmpty() && ("DEFAULT".equals(reason) || "DISABLED".equals(reason))) {
             resultBuilder = ProviderEvaluation.<ValT>builder().value(defaultValue);
         } else {
             resultBuilder = ProviderEvaluation.<ValT>builder().value(value).variant(variant);
